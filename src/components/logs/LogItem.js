9942efed7d8b25e899053f4ad8d7fa4ebcbca291
Abdul Mobeen <mobeendev@gmail.com>
2020-10-02 15:06:35 +0300

    loger and tech modal components

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,6 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Moment from "react-moment";
+import PropTypes from "prop-types";
 
 const LogItem = ({ log }) => {
   return (
