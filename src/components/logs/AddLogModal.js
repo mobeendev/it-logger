32c47f1569593a97f74d6954299549d2963e181f
Abdul Mobeen <mobeendev@gmail.com>
2020-10-04 21:18:19 +0300

    log & tech modal with redux

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
 import { connect } from "react-redux";
 import { addLog } from "../../actions/logActions";
+import TechSelectOptions from '../techs/TechSelectOptions';
 
 const AddLogModal = ({addLog}) => {
   const [message, setMessage] = useState("");
@@ -50,9 +51,7 @@ const AddLogModal = ({addLog}) => {
               <option value="" disabled>
                 Select Technician
               </option>
-              <option value="John Doe">John Doe</option>
-              <option value="Sam Smith">Sam Smith</option>
-              <option value="Sara Wilson">Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,59 +1,67 @@
-import React, { useState } from "react";
-import M from "materialize-css/dist/js/materialize.min.js";
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addTech } from '../../actions/techActions';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddTechModal = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+const AddTechModal = ({ addTech }) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
   const onSubmit = () => {
-    if (firstName === "" || lastName === "") {
-      M.toast({ html: "Please enter the first and last name" });
+    if (firstName === '' || lastName === '') {
+      M.toast({ html: 'Please enter the first and last name' });
     } else {
-      console.log(firstName, lastName);
+      addTech({
+        firstName,
+        lastName
+      });
+
+      M.toast({ html: `${firstName} ${lastName} was added as a tech` });
 
       // Clear Fields
-      setFirstName("");
-      setLastName("");
+      setFirstName('');
+      setLastName('');
     }
   };
 
   return (
-    <div id="add-tech-modal" className="modal">
-      <div className="modal-content">
+    <div id='add-tech-modal' className='modal'>
+      <div className='modal-content'>
         <h4>New Technician</h4>
-        <div className="row">
-          <div className="input-field">
+        <div className='row'>
+          <div className='input-field'>
             <input
-              type="text"
-              name="firstName"
+              type='text'
+              name='firstName'
               value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              onChange={e => setFirstName(e.target.value)}
             />
-            <label htmlFor="firstName" className="active">
+            <label htmlFor='firstName' className='active'>
               First Name
             </label>
           </div>
         </div>
 
-        <div className="row">
-          <div className="input-field">
+        <div className='row'>
+          <div className='input-field'>
             <input
-              type="text"
-              name="lastName"
+              type='text'
+              name='lastName'
               value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
+              onChange={e => setLastName(e.target.value)}
             />
-            <label htmlFor="lastName" className="active">
+            <label htmlFor='lastName' className='active'>
               Last Name
             </label>
           </div>
         </div>
       </div>
-      <div className="modal-footer">
+      <div className='modal-footer'>
         <a
-          href="#!"
+          href='#!'
           onClick={onSubmit}
-          className="modal-close waves-effect blue waves-light btn"
+          className='modal-close waves-effect blue waves-light btn'
         >
           Enter
         </a>
@@ -62,4 +70,11 @@ const AddTechModal = () => {
   );
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+  addTech: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { addTech }
+)(AddTechModal);
